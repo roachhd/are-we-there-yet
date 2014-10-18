e7b280f299152208b4b95768174c504a12ed2fdf
KB <katie@roachhd.com>
2014-10-19 00:58:10 +1100

    Game on mole 😛

---
+++ config.js
@@ -0,0 +1,19 @@
+/* Event time */
+var year 	= 2015;
+var month 	= 6;
+var day 	= 29;
+var hour 	= 10;
+var minute 	= 0;
+var second 	= 0;
+
+var daysMaxCount 	= 99;
+
+var finishText 		= "Game on Mole!" //Countdown reaches zero
+	
+/* Newsletter form tooltips */
+var blankEmail	= "this spot is for your email";
+var wrongEmail 	= "that kinda doesn't look right?";
+
+/* Google Analitycs */
+var googleAnalyticsId     = "UA-YOURGOOGS-ID";	// Google Analytics ID - e.g. UA-123456789-12
+var googleAnalyticsUrl    = "YOURURL.COM";		// Your domain name - e.g. muuthemes.pl
